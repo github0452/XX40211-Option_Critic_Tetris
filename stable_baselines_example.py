368ba6d7293371fd36d47d5af0c760c902752fb6
nlc29 <42607780+github0452@users.noreply.github.com>
2022-03-19 17:02:48 +0000

    Minor modifications

--- stable_baselines_example.py
+++ stable_baselines_example.py
@@ -4,28 +4,21 @@ from stable_baselines3 import PPO
 from stable_baselines3 import DQN
 from stable_baselines3.common.env_checker import check_env
 from tetris import TetrisEnv
+from option_critic import OptionCritic
 
-# env = gym.make("CartPole-v0")
-env = TetrisEnv(only_squares=True, grouped_actions=True, board_size=(3,4))
-# model = DQN("CnnPolicy", env, verbose=1,buffer_size=5000, tensorboard_log='logs/DQN')
-model = PPO("CnnPolicy", env, verbose=1, tensorboard_log='logs/PPO')
+# env = gym.make("Breakout-v0")
+env = TetrisEnv(only_squares=True, grouped_actions=True, board_size=(10,20), no_rotations=True)
+# model = DQN("CnnPolicy", env, verbose=0,buffer_size=10000, tensorboard_log='logs/DQN-tetris')
+model = PPO("CnnPolicy", env, verbose=0, tensorboard_log='logs/PPO-tetris')
+model = OptionCritic(env)
 # pre-training
-game_over = False
-obs = env.reset()
-while not game_over:
-    action, _states = model.predict(obs, deterministic=True)
-    obs, reward, game_over, info = env.step(action)
-    env.render()
-    print("Game over:", game_over)
+# game_over = False
+# obs = env.reset()
+# while not game_over:
+#     action, _states = model.predict(obs, deterministic=True)
+#     obs, reward, game_over, info = env.step(action)
+#     env.render()
+#     print("Game over:", game_over)
 
-for i in range(10):
-    model = model.learn(total_timesteps=100000, n_eval_episodes=20, reset_num_timesteps=False, log_interval=1)
-    model.save("dqn_cartpole")
-    # model = DQN.load("dqn_cartpole", env)
-    for _ in range(5):
-        game_over = False
-        obs = env.reset()
-        while not game_over:
-            action, _states = model.predict(obs, deterministic=True)
-            obs, reward, game_over, info = env.step(action)
-            env.render()
+model = model.learn(total_timesteps=10000000, log_interval=10)
+# model.save("ppo_tetris")
